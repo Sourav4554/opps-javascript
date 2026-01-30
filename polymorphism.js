fc2c03dff4509e086d1da2a6fcd7aa7844f19790
sourav <souravkrishna358@gmail.com>
2026-01-30 10:06:43 +0530

    inheritance

--- polymorphism.js
+++ polymorphism.js
@@ -1,10 +1,47 @@
-class Animal{
-sound(){
-console.log(`animals make sound`)
-}
-}
-class Dog extends Animal{
-sound(){
-console.log(`dog bark`)
-}
-}
+// class Animal{
+// sound(){
+// console.log(`animals make sound`)
+// }
+// }
+// class Dog extends Animal{
+// sound(){
+// console.log(`dog bark`)
+// }
+// }
+// const a=new Dog();
+// const b=new Animal()
+// b.sound()
+// a.sound()
+
+// class Vehicle{
+//   start(){
+// console.log('vehicle start')
+//  }
+
+// }
+// class Bike extends Vehicle{
+// start(){
+// console.log('bike starting')
+// }
+// }
+
+// const a=new Vehicle()
+// a.start()
+// const b=new Bike()
+// b.start()
+class Vehicle{
+    start(){
+  console.log('vehicle start')
+   }
+  
+  }
+  class Bike extends Vehicle{
+  start(){
+    super.start()
+  console.log('bike starting')
+  }
+  }
+  
+
+  const b=new Bike()
+  b.start()
