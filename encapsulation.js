ba33d9beeb546c6b36cd41b2640faec4e6d178cd
sourav <souravkrishna358@gmail.com>
2026-01-30 10:47:22 +0530

    encapsulation2

--- encapsulation.js
+++ encapsulation.js
@@ -20,6 +20,36 @@ class Student {
   }
   
   const s1 = new Student('alan', 56);
-  
   console.log(s1.Mark); 
+
+  //Create a BankAccount class where balance is private and can be
+  // accessed only using methods.
+  class BankAccount{
+    #balance;
+    constructor(bank,balance){
+    this.bank=bank,
+    this.#balance=balance
+    }
+    get Balance(){
+    return this.#balance
+    }
   
+    set Deposit(value){
+    if(value<0){
+    console.log('invalid')
+    }else{
+    this.#balance+=value
+    }
+    }
+     set Withdraw(value){
+        if(value<0){
+        console.log('invalid')
+        }else{
+        this.#balance-=value
+        }
+        }
+  }
+  const b=new BankAccount('FD',60000)
+  b.Deposit=1000
+  b.Withdraw=1
+  console.log(b.Balance)
